17320c176990a255f50792b9cf8db7558481fda7
unknown <kumaripriyanka1832@gmail.com>
2023-10-16 21:25:45 +0530

    textbox

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Textbox from './components/Textbox';
 import SendButton from './components/SendButton';
 function App() {
   return (
