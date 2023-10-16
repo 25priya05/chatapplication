f4705caaa9919b2a88ea45b545e0dadc52e10513
unknown <kumaripriyanka1832@gmail.com>
2023-10-16 12:41:57 +0530

    sendbutton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './components/Header';
+import SendButton from './components/SendButton';
 function App() {
   return (
     <div className="App">
       <Header/>
+      <SendButton/>
     </div>
   );
 }
